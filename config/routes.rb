56a4bac8914778b1de4b4fe167fff50402e60710
vagrant <vagrant@localhost.localdomain>
2020-10-05 10:40:34 +0000

    addfollow

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   root 'home#top'
 
   resources :posts
+  resources :users do
+    resource :relationship
+    resources :followings
+    resources :followers
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_04_103027) do
+ActiveRecord::Schema.define(version: 2020_10_05_073834) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2020_10_04_103027) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "target_user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["target_user_id"], name: "index_relationships_on_target_user_id"
+    t.index ["user_id"], name: "index_relationships_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
